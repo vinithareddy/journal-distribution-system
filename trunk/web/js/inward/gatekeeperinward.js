286136cdb7b0c6d049dbe1f8c96abe0207bbbef1
shailendra.mahapatra@gmail.com <shailendra.mahapatra@gmail.com@7c40806f-c66a-437e-5840-611a9df8ccbb>
2011-12-30 08:33:00 +0000

    gatekeeperinward

--- trunk/web/js/inward/gatekeeperinward.js
+++ trunk/web/js/inward/gatekeeperinward.js
@@ -38,3 +38,25 @@ function isInwardSelected(){
     return true;
 
 }
+
+
+function validateSearch(){
+
+    if(isEmpty(document.getElementById("inwardNumber"))
+        && isEmpty(document.getElementById("chequeNumber"))
+        && document.getElementById("city").value == "NULL"
+        && isEmpty(document.getElementById("from"))
+        && isEmpty(document.getElementById("to"))
+        ){
+        alert("Please select atleast one search criteria");
+        return false;
+    }else if(isEmpty(document.getElementById("from")) == false && isEmpty(document.getElementById("to"))){
+        alert("Please select both from and to dates in the range");
+        return false;
+    }else if(isEmpty(document.getElementById("to")) == false && isEmpty(document.getElementById("from"))){
+        alert("Please select both from and to dates in the range");
+        return false;
+    }
+
+    return true;
+}
