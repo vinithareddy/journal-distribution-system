d544c3ea6b7bc4717b4cfb1b7065e0ef602f4895
shailendra.mahapatra <shailendra.mahapatra@gmail.com>
2012-03-11 08:56:32 +0000

    Adding scripts to import data to mysql

---
+++ db/import_jds_data.bat
@@ -0,0 +1,2 @@
+"%PROGRAMFILES%\MySQL\MySQL Server 5.5\bin\mysql.exe" -u root -proot < generateschema.sql
+"%PROGRAMFILES%\MySQL\MySQL Server 5.5\bin\mysql.exe" -u root -proot -D jds < jds_schema_data.sql
