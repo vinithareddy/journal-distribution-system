34c3f8c1a74aae3b29f20840e3815c385ddec989
shailendra.mahapatra@gmail.com <shailendra.mahapatra@gmail.com@7c40806f-c66a-437e-5840-611a9df8ccbb>
2012-07-04 20:51:48 +0000

    new migrated data

---
+++ trunk/db/truncate_transaction_data.sql
@@ -0,0 +1,14 @@
+delete from inward;
+delete from subscriber;
+delete from subscriptiondetails;
+delete from subscription;
+delete from mailing_list;
+delete from mailing_list_detail;
+delete from back_issue_list;
+delete from reminder_details;
+delete from reminders;
+delete from missing_issue;
+delete from missing_issue_journals;
+delete from invoice;
+delete from payment;
+delete from agents;
