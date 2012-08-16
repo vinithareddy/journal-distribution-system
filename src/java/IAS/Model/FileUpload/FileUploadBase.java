3f666c681ffcf887ce7d0ae82db4cae5ec1a07d7
shailendra.mahapatra <shailendra.mahapatra@gmail.com>
2012-08-16 15:34:16 +0000

    file upload classes

--- src/java/IAS/Model/FileUpload/FileUploadBase.java
+++ src/java/IAS/Model/FileUpload/FileUploadBase.java
@@ -6,6 +6,8 @@ package IAS.Model.FileUpload;
 
 import IAS.Model.JDSModel;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import org.apache.commons.fileupload.FileItem;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
@@ -14,23 +16,36 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
  *
  * @author Newton
  */
-public class FileUploadBase extends JDSModel{
-    
-    private List<FileItem> uploadedFiles;
-    
-    public void addFiles(List<FileItem> _uploadedFiles){
-        uploadedFiles = _uploadedFiles;
+public class FileUploadBase extends JDSModel {
+
+    private ArrayList<FileItem> uploadedFiles=new ArrayList<>();
+
+    public void addFiles(List<FileItem> _uploadedFiles) {        
+        Iterator i = _uploadedFiles.iterator();
+        while (i.hasNext()) {
+            FileItem fileItem = (FileItem) i.next();
+            if (fileItem.isFormField() == false) {                
+                try{
+                    uploadedFiles.add(fileItem);
+                }catch(Exception e){
+                    int j = 0;
+                    
+                }
+                
+            }
+        }
     }
-    
-    public List<FileItem> getFiles(){
+  
+
+    public List<FileItem> getFiles() {
         return uploadedFiles;
     }
-    
-    public void processFiles() throws IOException{
-        throw(new NotImplementedException());
+
+    public void processFiles() throws IOException {
+        throw (new NotImplementedException());
     }
-    
-    public String getOutput(){
-        throw(new NotImplementedException());
+
+    public String getOutput() {
+        throw (new NotImplementedException());
     }
 }
