844f532573d2ee0fca17113a279fadf33f86a664
modak.alok@gmail.com <modak.alok@gmail.com@7c40806f-c66a-437e-5840-611a9df8ccbb>
2012-10-07 11:13:33 +0000

    Journal Rates report

--- trunk/src/java/IAS/Bean/Reports/subscriptionRatesFormBeanReport.java
+++ trunk/src/java/IAS/Bean/Reports/subscriptionRatesFormBeanReport.java
@@ -6,15 +6,26 @@ public class subscriptionRatesFormBeanReport extends JDSBean{
 
     /* fields */
 
-    private int year;
+    private int year=0;
     //private String colD;
+    private String subscriberType="";
+
     private String colN = "['Journal Group', 'Year', '1 Year']";
     private String colM = "["
             + "{name:'journalGroup', index:'journalGroup', xmlmap:'journalGroup'}, "
             + "{name:'year', index:'year', xmlmap:'year'},"
             + "{name:'1 Year', index:'1 Year', xmlmap:'1 Year'},"
             + "]";
+    private String data = "";
 
+public String getData() {
+
+    return(this.data);
+}
+
+public void setData(String _data) {
+    this.data = _data;
+}
 
 public String getColN() {
 
@@ -24,7 +35,6 @@ public String getColN() {
 public void setColN(String _colN) {
     this.colN = _colN;
 }
-
 
 public String getColM() {
 
@@ -42,5 +52,14 @@ public int getYear() {
 public void setYear(int _year) {
         this.year = _year;
     }
+
+public void setSubscriberType(String _subscriberType) {
+    this.subscriberType = _subscriberType;
+}
+
+public String getSubscriberType() {
+
+    return(this.subscriberType);
+}
 
 }
