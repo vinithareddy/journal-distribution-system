e7149d78cc7bde604997f1d76f0eb44bf02b298a
shailendra.mahapatra@gmail.com <shailendra.mahapatra@gmail.com@7c40806f-c66a-437e-5840-611a9df8ccbb>
2012-12-06 03:11:44 +0000

    invoice_types.sql

---
+++ JDSMigration/data/masterdata/invoice_types.sql
@@ -0,0 +1,6 @@
+USE jds;
+
+delete from `invoice_types`;
+ALTER TABLE `invoice_types` AUTO_INCREMENT = 1;
+
+insert into `invoice_types`(`invoice_type`) values ('Request For Invoice'),('Outstanding Payment'),('Upcoming Year Invoice')
