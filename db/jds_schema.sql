dc4c8e5e2e851c8a1408d0a2735d67a3fc1c25dc
shailendra.mahapatra <shailendra.mahapatra@gmail.com>
2013-01-14 08:17:21 +0000

    alter unique inward index in payment table add subgrid for subscription tab add receipt number in subscription tab

--- db/jds_schema.sql
+++ db/jds_schema.sql
@@ -564,7 +564,7 @@ CREATE TABLE `payment` (
   `remarks` varchar(20) DEFAULT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `unique_inward_id` (`inwardID`),
+  UNIQUE KEY `unique_inward_id` (`inwardID`,`invoice_id`),
   KEY `payment_idx1` (`invoice_id`),
   KEY `payment_idx2` (`inwardID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Save the relation between different inwards and a subscription. Subscriber can pay multiple times for the same subscription';
@@ -1349,4 +1349,4 @@ CREATE TABLE `year` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-01-14 12:29:16
+-- Dump completed on 2013-01-14 13:46:11
