ff66f19d99d82bf878605efb1edc7d5011317db7
shailendra.mahapatra <shailendra.mahapatra@gmail.com>
2013-05-19 16:17:39 +0000

    print invoice only when inward is present

--- web/js/invoice/subscriberInvoice.js
+++ web/js/invoice/subscriberInvoice.js
@@ -141,11 +141,11 @@ function drawInvoiceTable() {
                 //var invoice_no = jQuery("#invoiceTable").jqGrid('getCell', ids[i], 'InvoiceNo');
                 var invoice_type_id = jQuery("#invoiceTable").jqGrid('getCell', ids[i], 'invoice_type_id');
                 var inward_no = jQuery("#invoiceTable").jqGrid('getCell', ids[i], 'inwardNumber');
-                //var amountPaid = parseFloat(jQuery("#invoiceTable").jqGrid('getCell', ids[i], 'AmountPaid'));
+                var inwardNumber = jQuery("#invoiceTable").jqGrid('getCell', ids[i], 'inwardNumber');
                 var action = null;
 
                 // disable the view link for Please refer list invoices and if there are no payments made
-                if (invoice_type_id != jdsconstants.INVOICE_UPCOMING_YEAR_INVOICE) {
+                if (invoice_type_id != jdsconstants.INVOICE_UPCOMING_YEAR_INVOICE && inwardNumber) {
                     action = '<a style="color:blue;" href="#" onclick="showInvoice(\'' + inward_no + '\',' + invoice_type_id + ')">Print</a>';
                 }
                 jQuery("#invoiceTable").jqGrid('setRowData', ids[i], {
