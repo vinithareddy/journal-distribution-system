3e46b3e08c496856eeeb4fef9f16843a94bc48da
shailendra.mahapatra@gmail.com <shailendra.mahapatra@gmail.com@7c40806f-c66a-437e-5840-611a9df8ccbb>
2014-03-19 17:24:57 +0000

    add shipping address to cheque return

--- trunk/src/java/IAS/Class/ChequeReturnPDF.java
+++ trunk/src/java/IAS/Class/ChequeReturnPDF.java
@@ -4,11 +4,18 @@
  */
 package IAS.Class;
 
+import IAS.Bean.Subscriber.subscriberFormBean;
+import IAS.Model.Subscriber.subscriberModel;
 import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
+import java.sql.SQLException;
+import java.text.ParseException;
 import java.util.Properties;
 
 /**
@@ -51,9 +58,19 @@ public class ChequeReturnPDF extends JDSPDF{
         Paragraph paragraph = new Paragraph();
         Paragraph paragraph2 = new Paragraph();
         Paragraph paragraph3 = new Paragraph();
+        Paragraph paragraphAddress = new Paragraph();
+        Paragraph paragraphShippingAddress = new Paragraph();        
+        PdfPTable addressTable = new PdfPTable(1);
+        
+        //paragraphAddress.setAlignment(Element.ALIGN_LEFT);
+        paragraphAddress.setIndentationLeft(JDSPDF.LEFT_INDENTATION_LESS);
+        paragraphAddress.setSpacingBefore(JDSPDF.LESS_OUTER_PARAGRAPH_SPACE);
+        paragraphAddress.add(addressTable);
+        addressTable.setWidthPercentage(100);
+        
 
         //Font _IASFont = new Font(Font.FontFamily.HELVETICA, 11, Font.NORMAL, BaseColor.BLACK);
-        paragraph.setSpacingBefore(JDSPDF.OUTER_PARAGRAPH_SPACE);
+        paragraph.setSpacingBefore(JDSPDF.LESS_OUTER_PARAGRAPH_SPACE);
         paragraph.setIndentationLeft(JDSPDF.LEFT_INDENTATION_LESS);
         //paragraph.setFont(_IASFont);
         paragraph.setAlignment(Element.ALIGN_LEFT);
@@ -62,6 +79,73 @@ public class ChequeReturnPDF extends JDSPDF{
         //paragraph2.setFont(_IASFont);
         paragraph2.setSpacingBefore(JDSPDF.INNER_PARAGRAPH_SPACE);
         paragraph2.setAlignment(Element.ALIGN_LEFT);
+        
+        // create the shipping and invoice address table with 1 row and 2 cols
+        //paragraphShippingAddress.add(addressTable);
+        //paragraphShippingAddress.setIndentationLeft(JDSPDF.LEFT_INDENTATION_LESS);
+        //paragraphShippingAddress.setSpacingBefore(JDSPDF.LESS_OUTER_PARAGRAPH_SPACE);
+        
+        
+        Chunk shippingAddressHeader = new Chunk("SHIPPING ADDRESS", JDSPDF.JDS_FONT_BODY);
+        shippingAddressHeader.setTextRise(2);
+        shippingAddressHeader.setUnderline(1, 0);
+        paragraphShippingAddress.add(shippingAddressHeader);
+        paragraphShippingAddress.add(Chunk.NEWLINE);
+        subscriberFormBean subscriber = null;
+        try{
+            subscriberModel subscriber_model = new IAS.Model.Subscriber.subscriberModel();
+            subscriber = subscriber_model.GetSubscriber(SubscriberNumber);
+            
+        }catch(SQLException | ParseException | InvocationTargetException | IllegalAccessException | ClassNotFoundException ex){
+            throw new IOException(ex.getMessage());
+        }
+        
+        
+        paragraphShippingAddress.add(new Phrase(subscriber.getSubscriberName(), JDSPDF.JDS_FONT_BODY));
+        String _department = subscriber.getDepartment();
+        String _institute = subscriber.getInstitution();
+        String _shipping_address = subscriber.getShippingAddress();
+        String _city = subscriber.getCity();
+        String _country = subscriber.getCountry();
+        String _state = subscriber.getState();
+        int _pincode = subscriber.getPincode();
+
+        if (_department != null && _department.length() > 0) {
+            paragraphShippingAddress.add(Chunk.NEWLINE);
+            paragraphShippingAddress.add(new Phrase(_department, JDSPDF.JDS_FONT_BODY));
+        }
+        if (_institute != null && _institute.length() > 0) {
+            paragraphShippingAddress.add(Chunk.NEWLINE);
+            paragraphShippingAddress.add(new Phrase(_institute, JDSPDF.JDS_FONT_BODY));
+        }
+        if (_shipping_address.length() > 0) {
+            paragraphShippingAddress.add(Chunk.NEWLINE);
+            paragraphShippingAddress.add(new Phrase(_shipping_address, JDSPDF.JDS_FONT_BODY));
+        }
+        if (_city.length() > 0) {
+            paragraphShippingAddress.add(Chunk.NEWLINE);
+            paragraphShippingAddress.add(new Phrase(_city, JDSPDF.JDS_FONT_BODY));
+        }
+        if (_state.length() > 0) {
+            paragraphShippingAddress.add(Chunk.NEWLINE);
+            paragraphShippingAddress.add(new Phrase(_state, JDSPDF.JDS_FONT_BODY));
+        }
+        if (_country.length() > 0 && !_country.equalsIgnoreCase("india")) {
+            paragraphShippingAddress.add(Chunk.NEWLINE);
+            paragraphShippingAddress.add(new Phrase(_country, JDSPDF.JDS_FONT_BODY));
+        }
+        if (subscriber.getPincode() > 0) {
+            paragraphShippingAddress.add(Chunk.NEWLINE);
+            paragraphShippingAddress.add(new Phrase(String.valueOf(_pincode), JDSPDF.JDS_FONT_BODY));
+        }
+
+        PdfPCell shippingAddressCell = new PdfPCell(paragraphShippingAddress);
+        shippingAddressCell.setBorder(Rectangle.NO_BORDER);
+        shippingAddressCell.setPaddingLeft(0);
+        shippingAddressCell.setHorizontalAlignment(Element.ALIGN_LEFT);
+        shippingAddressCell.setVerticalAlignment(Element.ALIGN_TOP);        
+        addressTable.addCell(shippingAddressCell);
+        paragraph2.add(addressTable);
 
         paragraph3.setIndentationLeft(JDSPDF.LEFT_INDENTATION_LESS);
         //paragraph2.setFont(_IASFont);
