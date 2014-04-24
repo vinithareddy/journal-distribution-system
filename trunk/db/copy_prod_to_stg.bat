76322ea670c1058dc947d1825b591cfdea1fc8d7
shailendra.mahapatra@gmail.com <shailendra.mahapatra@gmail.com@7c40806f-c66a-437e-5840-611a9df8ccbb>
2014-04-24 18:37:36 +0000

    delete unnecessary files modify trigger edit_bil because a function was missing modify the existing invoice for a given subscription

--- trunk/db/copy_prod_to_stg.bat
+++
@@ -1,2 +0,0 @@
-"%PROGRAMFILES%\MySQL\MySQL Server 5.5\bin\mysqldump.exe" -h localhost -u root -proot evitaran | "%PROGRAMFILES%\MySQL\MySQL Server 5.5\bin\mysql.exe" -h localhost -u root -proot jds > prod_to_stage.log 2>&1
-pause
