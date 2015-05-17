50fce4e8729b863bc85d6077ed1f828fe0ec74c5
modak.alok@gmail.com <modak.alok@gmail.com@7c40806f-c66a-437e-5840-611a9df8ccbb>
2015-05-17 06:54:06 +0000

    JDSMigration moved to trunk for GitHub migration

--- JDSMigration/data/masterdata/invoice_types.sql
+++
@@ -1,6 +0,0 @@
-USE evitaran;
-
-truncate table `invoice_types`;
-ALTER TABLE `invoice_types` AUTO_INCREMENT = 1;
-
-insert into `invoice_types`(`invoice_type`) values ('Request For Invoice'),('Outstanding Payment'),('Upcoming Year Invoice')
